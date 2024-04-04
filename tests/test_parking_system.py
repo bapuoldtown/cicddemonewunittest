012c6ac5828d09ec8cbedb59708bc5129b1628a4
bapuoldtown <bapucodetest1987@gmail.com>
2024-04-04 23:32:09 +0530

    push to main to trigger action flows

---
+++ tests/test_parking_system.py
@@ -0,0 +1,15 @@
+import pytest
+from app_codes.parking_system import ParkingSystem
+
+@pytest.mark.component
+def test_reserve_mid():
+    sob = ParkingSystem("grand_i10", "mid")
+    sob.reserve_mid()
+    assert ParkingSystem.display() is not None
+    
+
+@pytest.mark.component
+def test_reserve_mid():
+    sob = ParkingSystem("isuzu", "large")
+    sob.reserve_large()
+    assert ParkingSystem.display() is not None 
