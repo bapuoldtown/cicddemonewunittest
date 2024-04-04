07d3e1e50605db61686ae96b6dce77d8be916268
bapuoldtown <bapucodetest1987@gmail.com>
2024-04-05 00:06:41 +0530

    flake 8 formatting for each category

--- app_codes/parking_system.py
+++ app_codes/parking_system.py
@@ -1,17 +1,23 @@
 from collections import defaultdict
+
+
 class ParkingSystem:
     vehicle_list = defaultdict(list)
+
     def __init__(self, name: str, typ: str):
         self.name = name
         self.typ = typ
+
     def reserve_mid(self):
-        self.vehicle_list["mid_vehicle"].append({self.name : self.typ})
+        self.vehicle_list["mid_vehicle"].append({self.name: self.typ})
+
     def reserve_large(self):
-        self.vehicle_list["large_vehicle"].append({self.name : self.typ})
+        self.vehicle_list["large_vehicle"].append({self.name: self.typ})
+
     @classmethod
     def display(cls):
         return dict(cls.vehicle_list)
-        
+
 
 sob = ParkingSystem("grand_i10", "mid")
 lob = ParkingSystem("isuzu max", "large")
--- tests/test_parking_system.py
+++ tests/test_parking_system.py
@@ -1,15 +1,16 @@
 import pytest
 from app_codes.parking_system import ParkingSystem
 
+
 @pytest.mark.component
 def test_reserve_mid():
     sob = ParkingSystem("grand_i10", "mid")
     sob.reserve_mid()
     assert ParkingSystem.display() is not None
-    
+
 
 @pytest.mark.component
-def test_reserve_mid():
+def test_reserve_large():
     sob = ParkingSystem("isuzu", "large")
     sob.reserve_large()
-    assert ParkingSystem.display() is not None 
+    assert ParkingSystem.display() is not None
